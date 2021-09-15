c89ac554c5e18e5908051e8eadf9e0c25961b958
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-09-14 23:30:14 -0300

    fix: alterado nome de verbose name da app

--- medicar/specialties/apps.py
+++ medicar/specialties/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig
 class SpecialtiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'medicar.specialties'
+    verbose_name = "especialidades"
 
